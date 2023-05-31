c29cec85ad6fc524916c3e0a7fd6836a819225ae
Sahinshf <shahinrsha@code.edu.az>
2023-05-31 08:15:43 +0400

    Blog section is completed , Admin can chamges users active status

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Project.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Project/Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿namespace Project.Models;
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Image { get; set; }
+    public string Name { get; set; }    
+    public string Description { get; set; }
+    public string Author { get; set; }
+    public int Comment { get; set; }
+    public DateTime Created { get; set; }
+    public bool IsDeleted { get; set; }
+}
