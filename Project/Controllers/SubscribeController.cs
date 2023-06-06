237bdf818a75b871a62bb0c7ee605f220f839f33
Sahinshf <shahinrsha@code.edu.az>
2023-06-06 17:50:20 +0400

    Login Logout

--- Project/Controllers/SubscribeController.cs
+++ Project/Controllers/SubscribeController.cs
@@ -1,47 +1,47 @@
-﻿using System.Web;
-using Microsoft.EntityFrameworkCore;
+﻿//using System.Web;
+//using Microsoft.EntityFrameworkCore;
 
-namespace Project.Controllers;
+//namespace Project.Controllers;
 
-public class SubscribeController : Controller
-{
+//public class SubscribeController : Controller
+//{
 
-    private readonly AppDbContext _context;
+//    private readonly AppDbContext _context;
 
-    public SubscribeController(AppDbContext context)
-    {
-        _context = context;
-    }
+//    public SubscribeController(AppDbContext context)
+//    {
+//        _context = context;
+//    }
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Subscribe(string email)
-    {
-        string previousUrl = Request.Headers["Referer"].ToString();
+//    [HttpPost]
+//    [ValidateAntiForgeryToken]
+//    public async Task<IActionResult> Subscribe(string email)
+//    {
+//        string previousUrl = Request.Headers["Referer"].ToString();
 
-        var emails = await _context.Subscribes.ToListAsync();
+//        var emails = await _context.Subscribes.ToListAsync();
 
-        foreach (var item in emails)
-        {
+//        foreach (var item in emails)
+//        {
 
-            if (item.Email.Trim().ToUpper() == email.Trim().ToUpper())
-            {
+//            if (item.Email.Trim().ToUpper() == email.Trim().ToUpper())
+//            {
 
-                ModelState.AddModelError("email", "Email already exists.");
-                return BadRequest("Email already exists.");
+//                ModelState.AddModelError("email", "Email already exists.");
+//                return BadRequest("Email already exists.");
 
-            }
-        }
-
-
-        Subscribe subscribe = new() { Email = email };
+//            }
+//        }
 
 
-        await _context.Subscribes.AddAsync(subscribe);
-        await _context.SaveChangesAsync();
+//        Subscribe subscribe = new() { Email = email };
 
 
-        return Redirect(previousUrl);
+//        await _context.Subscribes.AddAsync(subscribe);
+//        await _context.SaveChangesAsync();
 
-    }
-}
+
+//        return Redirect(previousUrl);
+
+//    }
+//}
